dcc8b2738e9556dec32a5719b05949839440ed71
Do Hoang Viet <vietdh92@gmail.com>
2020-05-07 14:49:33 +0700

    Init project

---
+++ AdvanceSwift/AdvanceSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdvanceSwift
+//
+//  Created by Gia Han on 5/7/20.
+//  Copyright © 2020 vn.netstars. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
