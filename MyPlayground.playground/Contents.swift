01773e3c08a36959518e11a356485e92d82fe2ef
Do Hoang Viet <vietdh92@gmail.com>
2020-05-08 14:16:55 +0700

    Known Sequence Map

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,22 @@
 import UIKit
 
-var str = "Hello, playground"
-print(str)
+let numbers = [1, 2, 3, 4, 5]
+let doubled = numbers.map { $0 * 2 }
+print(doubled)
+
+let strings = ["Apples", "Banana", "Pear"]
+let uppercased = strings.map { $0.uppercased() }
+
+print(uppercased)
+
+extension Sequence {
+    public func map2<T>(_ transform: (Element) -> T) -> [T] {
+        var result = [T]()
+        for item in self {
+            result.append(transform(item))
+        }
+        return result
+    }
+}
+
+print(strings.map2 { $0.uppercased() })
