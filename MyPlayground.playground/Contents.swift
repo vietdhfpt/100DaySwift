cbe0e43f2f04a0dbe8cf39527cc08da92006ea7e
Do Hoang Viet <vietdh92@gmail.com>
2020-05-18 16:15:03 +0700

    Using Semaphore wait and signal, Dispatch Queue

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,48 @@
 import UIKit
 
-var str = "Hello, playground"
-print(str)
+DispatchQueue.main.async {
+    print("operation 1")
+    DispatchQueue.main.async {
+        print("operation 1.1")
+    }
+}
+
+DispatchQueue.main.async {
+    print("operation 2")
+}
+
+
+func run(queue: DispatchQueue, times: Int) {
+    (0..<times).forEach { i in
+        queue.async {
+            print("\(i) operation 1")
+            queue.async {
+                print("\(i) operation 1.1")
+            }
+        }
+
+        queue.async {
+            print("\(i) operation 2")
+        }
+    }
+}
+run(queue: .main, times: 3)
+
+let queue = DispatchQueue(label: "com.run", attributes: .concurrent)
+run(queue: queue, times: 3)
+
+
+let myQueue = DispatchQueue(label: "com.gcd.myQueue", attributes: .concurrent)
+let semaphore = DispatchSemaphore(value: 1)
+for i in (0...14) {
+  myQueue.async {
+     let songNumber = i + 1
+     semaphore.wait()
+     print("Downloading song", songNumber)
+     sleep(2) // Download take ~2 sec each
+     print("Downloaded song", songNumber)
+     semaphore.signal()
+  }
+}
+
+
